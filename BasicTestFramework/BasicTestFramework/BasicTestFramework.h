083a73ce3ef8c9bbf2f4ff2ca88992cfcf3acdbf
Ismail Bozkurt <ibozkurt@hubspot.com>
2024-10-05 15:30:13 +0100

    init

---
+++ BasicTestFramework/BasicTestFramework/BasicTestFramework.h
@@ -0,0 +1,18 @@
+//
+//  BasicTestFramework.h
+//  BasicTestFramework
+//
+//  Created by Ismail Bozkurt on 05/10/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BasicTestFramework.
+FOUNDATION_EXPORT double BasicTestFrameworkVersionNumber;
+
+//! Project version string for BasicTestFramework.
+FOUNDATION_EXPORT const unsigned char BasicTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasicTestFramework/PublicHeader.h>
+
+
---
+++ BasicTestFramework/BasicTestFramework/File.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+public typealias TTTTTTestAttachment = XCTAttachment
+
+public struct TTTTT: Equatable {
+    let abc: Int = 3
+}
